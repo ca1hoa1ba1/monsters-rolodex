291ba6369c53c33d47a9b4077893b4e9954b7b23
ca1hoa1ba1 <ca1hoa1ba1@gmail.com>
2019-07-27 10:44:55 +0700

    Updates

---
+++ precache-manifest.8767cda6006ec357870dbc7eb62410b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d38601b67f3b1ee76665f8060dc83e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4693ca347014c8af62ae",
+    "url": "/monsters-rolodex/static/css/main.6a1865cc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4693ca347014c8af62ae",
+    "url": "/monsters-rolodex/static/js/main.dfdcb844.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
